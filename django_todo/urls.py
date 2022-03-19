b3cbc04ff28c0cd4edfb7c7f22786ace86e3d355
robindavidgames <robin.david.games@gmail.com>
2022-03-19 15:58:58 +0000

    CRU of CRUD

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 # from todo.views import say_hello
-from todo.views import get_todo_list, add_item
+from todo.views import get_todo_list, add_item, edit_item
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +24,6 @@ urlpatterns = [
     # this means that if I open the browser and apend /hello it will run
     # say_hello
     path('', get_todo_list, name='get_todo_list'),
-    path('add', add_item, name='add')
+    path('add', add_item, name='add'),
+    path('edit/<item_id>', edit_item, name='edit')
 ]
